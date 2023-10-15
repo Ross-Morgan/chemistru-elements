ec7973544740a6132411c4dec3d6d6de93d72650
Ross-Morgan <rmorgan512@protonmail.ch>
2023-10-15 12:00:57 +0100

    add ops module

---
+++ src/ops.rs
@@ -0,0 +1,11 @@
+use super::element::Element;
+
+use std::cmp::PartialEq;
+
+impl PartialEq for Element {
+    fn eq(&self, other: &Self) -> bool {
+        self.atomic_mass() == other.atomic_mass()
+        &&
+        self.atomic_number() == other.atomic_number()
+    }
+}
