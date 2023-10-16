2f6142eb467a87baf1a19645b0887c56df5e74cd
Ross-Morgan <rmorgan512@protonmail.ch>
2023-10-16 13:44:12 +0100

    add equality checking for elements

--- src/ops.rs
+++ src/ops.rs
@@ -4,8 +4,6 @@ use std::cmp::PartialEq;
 
 impl PartialEq for Element {
     fn eq(&self, other: &Self) -> bool {
-        self.atomic_mass() == other.atomic_mass()
-        &&
-        self.atomic_number() == other.atomic_number()
+        self.atomic_mass() == other.atomic_mass() && self.atomic_number() == other.atomic_number()
     }
 }
